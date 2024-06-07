089685c77822ba149dfa059b733d9a872111d5c9
이종경 <gyoung3063413@naver.com>
2024-06-07 17:29:53 +0900

    fix: signup 페이지 이동이 안되는 현상 수정

--- src/app/(beforeLogin)/_component/Main.tsx
+++ src/app/(beforeLogin)/_component/Main.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
-        <Link href='/signup' className={styles.signup}>
+        <Link href='/i/flow/signup' className={styles.signup}>
           계정 만들기
         </Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
