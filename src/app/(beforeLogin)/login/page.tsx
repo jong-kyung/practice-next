1caf9fe60a08cbcdc7aa14fd9a1a81c37fbe43a6
이종경 <gyoung3063413@naver.com>
2024-06-07 17:18:33 +0900

    Feat: 회원가입 모달창 생성

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,16 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-  redirect("/i/flow/login");
+  const router = useRouter();
+  router.replace("/i/flow/login");
+  return null;
 }
+
+// router.push
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+
+// router.replace
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+// localhost:3000 -> localhost:3000/i/flow/login
