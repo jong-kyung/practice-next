48899b478f4e951800fc4bea897f08ab98511f5a
이종경 <gyoung3063413@naver.com>
2024-06-10 16:20:22 +0900

    design: 게시글 작성 폼 디자인 적용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,12 +2,14 @@ import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 
 import style from "./home.module.css";
+import PostForm from "./_component/PostForm";
 
 export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tab />
+        <PostForm />
       </TabProvider>
     </main>
   );
