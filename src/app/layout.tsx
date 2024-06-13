60075e4a4c5dd08d2341eca70f74d097e033161d
이종경 <gyoung3063413@naver.com>
2024-06-13 16:19:23 +0900

    feat: msw 설치 및 적용 - next 대응 임시 서버 생성 - handlers 테스트 파일 생성 - MSWComponent 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { MSWComponent } from "@/app/_component/MSWComponent";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +17,11 @@ type Props = {
 
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='ko'>
+      <body className={inter.className}>
+        <MSWComponent />
+        {children}
+      </body>
     </html>
   );
 }
