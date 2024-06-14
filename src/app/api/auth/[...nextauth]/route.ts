ca09c8843388d6230caf1d46e1383d7ed43fd610
이종경 <gyoung3063413@naver.com>
2024-06-14 15:55:39 +0900

    feat: 로그인 기능 구현

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+export { GET, POST } from "@/auth";
+// Catch all routes
+// GET /api/auth/a
+// GET /api/auth/b
+// GET /api/auth/a/b/c
---
+++ src/auth.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  pages: {
+    signIn: "/auth/signin", // 로그인 페이지등록
+    newUser: "/auth/signup", // 회원가입 페이지등록
+  },
+  providers: [
+    Credentials({
+      authorize: async (credentials) => {
+        const authResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            // username과 password로 설정이 되어 있어서 id와 password로 변경함.
+            id: credentials.username,
+            password: credentials.password,
+          }),
+        });
+
+        if (!authResponse.ok) {
+          return null;
+        }
+
+        const user = await authResponse.json();
+        console.log("user", user);
+        return {
+          email: user.id,
+          name: user.nickname,
+          image: user.image,
+          ...user,
+        };
+      },
+    }),
+  ],
+});
