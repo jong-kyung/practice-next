06a192495191d74ca26a5b22cb191db337257de9
이종경 <gyoung3063413@naver.com>
2024-06-18 21:26:20 +0900

    fix: 사용자 상세페이지 queryFn 변경

--- src/app/(afterLogin)/[username]/_component/UserPosts.tsx
+++ src/app/(afterLogin)/[username]/_component/UserPosts.tsx
@@ -2,9 +2,9 @@
 
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { getPostRecommends } from "@/app/(afterLogin)/home/_lib/getPostRecommends";
 import Post from "../../_component/Post";
 import { Post as IPost } from "@/model/Post";
+import { getUserPosts } from "../_lib/getUserPosts";
 
 type Props = {
   username: string;
@@ -13,9 +13,9 @@ type Props = {
 export default function UserPosts({ username }: Props) {
   const { data } = useQuery<IPost[], Object, IPost[], [_1: string, _2: string, _3: string]>({
     queryKey: ["posts", "users", username],
-    queryFn: getPostRecommends,
-    staleTime: 60 * 1000, // 1분
-    gcTime: 300 * 1000, // 5분
+    queryFn: getUserPosts,
+    staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+    gcTime: 300 * 1000,
   });
 
   const queryClient = useQueryClient();
